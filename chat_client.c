ddb8ce2f05122242a70eecde3d277e9f277e7f6b
an_thony350 <afob2@hotmail.com>
2025-11-20 21:25:18 +0000

    [update] finished sender thread function and main for chat_client

--- chat_client.c
+++ chat_client.c
@@ -5,63 +5,73 @@
 #include <pthread.h>
 #include "udp.h"
 
-#define CLIENT_PORT 10000
-#define SERVER_PORT 20000
-#define BUFFER_SIZE 1024
-#define SERVER_IP "127.0.0.1"
-
-// Global Socket + Server Address
-
-int sd;
-struct sockaddr_in server_addr;
 
 // Listener Thread Function
-void *listener_thread_func(void *arg)
-{ 
-    char server_response[BUFFER_SIZE];
+void *listener_thread(void *arg)
+{
+    int sd = *(int *)arg;
+    char buffer[BUFFER_SIZE];
     struct sockaddr_in responder_addr;
 
     while (1)
     {
-
-        int rc = udp_socket_read(sd, &responder_addr, server_response, BUFFER_SIZE);
-
+        int rc = udp_socket_read(sd, &responder_addr, buffer, BUFFER_SIZE);
         if (rc > 0)
         {
-            buffer[rc] = '\0'; // Null-terminate the received string
-            printf("%s\n", server_response);
+            buffer[rc] = '\0';
+            printf("%s\n", buffer);
             fflush(stdout);
         }
     }
     return NULL;
 }
 
+// Sender Thread Function
+void *sender_thread(void *arg)
+{
+    struct sender_args {
+        int sd;
+        struct sockaddr_in addr;
+    };
 
+    struct sender_args *args = arg;
 
+    int sd = args->sd;
+    struct sockaddr_in server_addr = args->addr;
 
+    char client_request[BUFFER_SIZE];
 
-// client code
+    while (1)
+    {
+        // Read input from user
+        if (fgets(client_request, BUFFER_SIZE, stdin) == NULL)
+            continue;
+
+        client_request[strcspn(client_request, "\n")] = '\0';
+
+        if (strlen(client_request) == 0)
+            continue;
+
+        // Send the raw request to the server
+        udp_socket_write(sd, &server_addr, client_request, BUFFER_SIZE);
+
+        // If the user typed disconn$, quit the client
+        if (strncmp(client_request, "disconn$", 8) == 0)
+        {
+            exit(0);
+        }
+    }
+
+    return NULL;
+}
+
 int main(int argc, char *argv[])
 {
-    // This function opens a UDP socket,
-    // binding it to all IP interfaces of this machine,
-    // and port number CLIENT_PORT.
-    // (See details of the function in udp.h)
     int sd = udp_socket_open(CLIENT_PORT);
+    assert(sd > -1);
 
-    // Variable to store the server's IP address and port
-    // (i.e. the server we are trying to contact).
-    // Generally, it is possible for the responder to be
-    // different from the server requested.
-    // Although, in our case the responder will
-    // always be the same as the server.
     struct sockaddr_in server_addr, responder_addr;
 
-    // Initializing the server's address.
-    // We are currently running the server on localhost (127.0.0.1).
-    // You can change this to a different IP address
-    // when running the server on a different machine.
-    // (See details of the function in udp.h)
     int rc = set_socket_addr(&server_addr, "127.0.0.1", SERVER_PORT);
 
     // Storage for request and response messages
