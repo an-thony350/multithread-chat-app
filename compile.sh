4c1a3d199ce892dd0221042fb9220e91f380dd63
an_thony350 <afob2@hotmail.com>
2025-11-21 14:25:20 +0000

    [bugfix} added kill command to compile.sh as code would just run old server

--- compile.sh
+++ compile.sh
@@ -1,5 +1,9 @@
+pkill chat_server
+pkill chat_client
+
 rm chat_server
 rm chat_client
 
-gcc chat_server.c -o chat_server
-gcc chat_client.c -o chat_client
+gcc chat_server.c -o chat_server -pthread
+gcc chat_client.c -o chat_client -pthread
+
