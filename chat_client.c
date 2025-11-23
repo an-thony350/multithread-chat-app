8d750d37133471306a1fb34454dbffc2e58a4fd4
an_thony350 <afob2@hotmail.com>
2025-11-23 13:18:11 +0000

    [feature] added scrolling through chat history and improved UI design

--- chat_client.c
+++ chat_client.c
@@ -7,8 +7,15 @@
 #include "udp.h"
 
 #define CLIENT_PORT 55555
-WINDOW *chat_win;
+WINDOW *chat_pad;
 WINDOW *input_win;
+
+int chat_lines = 0;
+int scroll_offset = 0;
+volatile int should_exit = 0;
+
+void redraw_pad();
+
 
     struct sender_args {
         int sd;
@@ -24,12 +31,24 @@ void *listener_thread(void *arg)
 
     while (1)
     {
+        if (should_exit) return NULL;
+        
         int rc = udp_socket_read(sd, &responder_addr, buffer, BUFFER_SIZE);
         if (rc > 0)
         {
             buffer[rc] = '\0';
-            wprintw(chat_win, "%s\n", buffer);
-            wrefresh(chat_win);
+            mvwprintw(chat_pad, chat_lines++, 0, "%s", buffer);
+
+            int rows, cols;
+            getmaxyx(stdscr, rows, cols);
+
+            pnoutrefresh(
+                chat_pad, 
+                (chat_lines > rows - 2 ? chat_lines - (rows - 2) : 0), 
+                0, 
+                0, 0, 
+                rows - 3, cols - 1
+            );
 
             wrefresh(input_win);
         }
@@ -41,6 +60,7 @@ void *listener_thread(void *arg)
 void *sender_thread(void *arg)
 {
 
+
     struct sender_args *args = arg;
 
     int sd = args->sd;
@@ -51,7 +71,62 @@ void *sender_thread(void *arg)
     while (1)
     {
         // Read input from user
-        wgetnstr(input_win, client_request, BUFFER_SIZE);
+        int ch;
+        int input_pos = 0;
+        client_request[0] = '\0';
+        
+        while ((ch = wgetch(input_win)))
+        {
+            if (ch == KEY_DOWN) {
+                if (scroll_offset > 0) 
+                scroll_offset--;
+                redraw_pad();
+                continue;
+                
+            }
+            if (ch == KEY_UP) {
+                if (scroll_offset < chat_lines - 1) 
+                scroll_offset++;
+                redraw_pad();
+                continue;
+            }
+            if (ch == '\n') break;
+
+            if (ch == KEY_BACKSPACE || ch == 127) {
+                if (input_pos > 0) {
+                    input_pos--;
+                    client_request[input_pos] = '\0';
+                }
+                wmove(input_win, 0, 2);
+                wclrtoeol(input_win);
+                wprintw(input_win, "%s", client_request);
+                wrefresh(input_win);
+                continue;
+            }
+
+            if (input_pos < BUFFER_SIZE - 1) {
+                client_request[input_pos++] = ch;
+                client_request[input_pos] = '\0';
+                wclrtoeol(input_win);
+                wrefresh(input_win);
+            }
+            if (ch == KEY_MOUSE) {
+                MEVENT event;
+                if (getmouse(&event) == OK) {
+                    if (event.bstate & BUTTON5_PRESSED) {
+                        if (scroll_offset > 0) 
+                        scroll_offset--;
+                        redraw_pad();
+                    } else if (event.bstate & BUTTON4_PRESSED) {
+                        if (scroll_offset < chat_lines) 
+                        scroll_offset++;
+                        redraw_pad();
+                    }
+                }
+            }
+
+
+        }
 
 
         client_request[strcspn(client_request, "\n")] = '\0';
@@ -69,12 +144,30 @@ void *sender_thread(void *arg)
         // If the user typed disconn$, quit the client
         if (strncmp(client_request, "disconn$", 8) == 0)
         {
-            exit(0);
+            should_exit = 1;
+            return NULL;
         }
     }
 
     return NULL;
 }
+
+void redraw_pad() {
+    int rows, cols;
+    getmaxyx(stdscr, rows, cols);
+
+    int view_top = chat_lines - (rows - 2) - scroll_offset;
+    if (view_top < 0) view_top = 0;
+
+    pnoutrefresh(
+        chat_pad,
+        view_top, 0,
+        0, 0,
+        rows - 3, cols - 1
+    );
+    doupdate();
+}
+
 
 int main(int argc, char *argv[])
 {
@@ -93,6 +186,7 @@ int main(int argc, char *argv[])
     args.addr = server_addr;
 
     initscr();
+    mousemask(ALL_MOUSE_EVENTS, NULL);
     cbreak();
     noecho();
     curs_set(1);
@@ -100,11 +194,15 @@ int main(int argc, char *argv[])
     int rows, cols;
     getmaxyx(stdscr, rows, cols);
 
-    chat_win = newwin(rows - 1, cols, 0, 0);
-    scrollok(chat_win, TRUE);
+    chat_pad = newpad(5000, cols);  // 5000 lines buffer (increase anytime)
+    scrollok(chat_pad, TRUE);
+
     input_win = newwin(1, cols, rows - 1, 0);
     wprintw(input_win, "> ");
     wrefresh(input_win);
+
+    mvhline(rows - 2, 0, '=', cols);
+    wrefresh(stdscr);
 
     keypad(input_win, TRUE);
     wmove(input_win, 0, 2);  
