bef2723399763dd5ec7276813a19ccef478502a2
an_thony350 <afob2@hotmail.com>
2025-12-03 13:28:00 +0000

    [update] improved code readability

--- compile.sh
+++ compile.sh
@@ -1,8 +1,10 @@
+# Kill any running instances of the chat server and client & Remove old binaries
 pkill chat_server
 pkill chat_client
 
 rm chat_server
 rm chat_client
 
+# Compile the chat server and client
 gcc chat_server.c -o chat_server 
 gcc chat_client.c -o chat_client -lncurses -lpthread
